2524c2fda16df66ed1104153ff4c7ce59eb52564
Victor <rusvictor@hotmail.es>
2023-05-10 16:04:45 +0200

    new repositry

---
+++ src/main/java/com/victorsashnev/tema12/Main.java
@@ -0,0 +1,10 @@
+package com.victorsashnev.tema12;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+    //ordenador de profe
+    File f = new File(File.separator +"home" + File.separator +"gacon" +File.separator +" hola.txt");
+    }
+}
